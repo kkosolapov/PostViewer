46114df719abecf9686b7e43f1c3bd1885319be9
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-07-01 16:27:38 +0300

    Implement tableView function

--- Controllers/MainViewController.swift
+++ Controllers/MainViewController.swift
@@ -115,3 +115,58 @@ class MainViewController: UIViewController {
   }
   
 }
+
+
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return postModel?.count ?? 0
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: NewsTableViewCell.identifier, for: indexPath) as? NewsTableViewCell else { fatalError() }
+    if let model = postModel?[indexPath.row] {
+      cell.configure(with: model)
+    }
+    
+    // Adding/Removing expanded cells to Set
+    cell.buttonTapBlock = { [weak self] in
+      guard let self = self else { return }
+      if self.expandedIndexSet.contains(indexPath.row) {
+        self.expandedIndexSet.remove(indexPath.row)
+      } else {
+        self.expandedIndexSet.insert(indexPath.row)
+      }
+      tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+    
+    //  Cell configuration depends of their 2 states (Expand/Collapse)
+    if expandedIndexSet.contains(indexPath.row) {
+      cell.previewLabel.numberOfLines = 0
+      cell.configureButton(withTitle: "Collapse")
+    } else {
+      cell.previewLabel.numberOfLines = 2
+      cell.configureButton(withTitle: "Expand")
+    }
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    
+    // Passing data to DetailViewController
+    let detailVC = DetailViewController()
+    if let model = postModel?[indexPath.row] {
+      detailVC.id = model.postId
+      detailVC.newsTitle = model.title
+      detailVC.newsText = model.previewText
+      detailVC.likes = String(model.likesCount)
+      detailVC.date = model.timestamp.toString()
+    }
+    
+    navigationController?.pushViewController(detailVC, animated: true)
+  }
+}
