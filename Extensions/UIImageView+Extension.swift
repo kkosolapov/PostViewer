e89bdaa5810807d03e94b15ebd21bfad027b2598
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-07-02 07:48:45 +0300

    Implement loadFrom function

---
+++ Extensions/UIImageView+Extension.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+extension UIImageView {
+  
+  func loadFrom(URLAddress: String) {
+    guard let url = URL(string: URLAddress) else { return }
+    
+    DispatchQueue.main.async { [weak self] in
+      if let imageData = try? Data(contentsOf: url) {
+        if let loadedImage = UIImage(data: imageData) {
+          self?.image = loadedImage
+        }
+      }
+    }
+  }
+}
